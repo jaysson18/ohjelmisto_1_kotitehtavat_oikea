10a1ce2b788fc0dc6996e4f7855d890f7655405d
jaysson18 <jahaka999@gmail.com>
2023-08-23 15:53:17 +0300

    Tehtava

---
+++ Moduuli_3/tehtava_3-1.py
@@ -0,0 +1,18 @@
+def main():
+    try:
+        kuhan_pituus = float(input("Anna kuhan pituus senttimetreinä: "))
+
+        alin_sallittu_pituus = 37.0
+
+        if kuhan_pituus < alin_sallittu_pituus:
+            puuttuva_pituus = alin_sallittu_pituus - kuhan_pituus
+            print(f"Kuha on alimittainen. Laske se takaisin järveen. Puuttuu {puuttuva_pituus: .2f} cm.")
+        else:
+            print(f"Kuha on yli alimman sallitun pyyntimitan. Voit pitää sen.")
+
+
+    except ValueError:
+        print(f"Antamasi arvo ei ole kelvollinen numero. Yritä uudestaan.")
+
+if __name__ == "__main__":
+    main()
